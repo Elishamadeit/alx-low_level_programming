ac3003219dcecb740fbb418e940821bc8fb5b764
Julius <elishajnr8@gmail.com>
2022-10-14 00:42:21 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main-program entry point
+  *
+  *Return:Returns zero if successful otherwise a non-zero
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
