31f76d278c51336d154ee1274bb430b897eaaefd
Julius <elishajnr8@gmail.com>
2022-10-14 15:01:39 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main-program entry point.
+  *Return:0 if there is no error, a non zero if error.
+  **/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
